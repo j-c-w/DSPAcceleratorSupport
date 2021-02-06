1ffa8e32581827588d4b541ecf5f75210d74c264
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-02-06 23:06:51 +0000

    Actually generate programs in generate_programs

--- synth/generate_programs.ml
+++ synth/generate_programs.ml
@@ -1,7 +1,19 @@
 open Core_kernel;;
 open Spec_definition;;
 open Gir;;
+open Gir_utils;;
 open Options;;
+
+let add_all_types_to frommap tomap =
+    ignore(Hashtbl.iter_keys frommap (fun (apitypekey: string) ->
+        ignore(Hashtbl.add tomap apitypekey (Hashtbl.find_exn frommap apitypekey))))
+
+let build_typemap_for (apispec: apispec) (iospec: iospec) gir =
+    let newtbl: (string, synth_type) Hashtbl.t =
+        Hashtbl.create (module String) in
+    let () = add_all_types_to apispec.typemap newtbl in
+    let () = add_all_types_to iospec.typemap newtbl in
+    newtbl
 
 let generate_program_for (apispec: apispec) (iospec: iospec) (pre, post) =
 	(* Generate the function call to the API.  *)
@@ -12,7 +24,7 @@ let generate_program_for (apispec: apispec) (iospec: iospec) (pre, post) =
 	   targetting C.  More for functional/tuple languages
 	   would not be hard.  *)
 	let rettype = Option.map iospec.returnvar (fun v -> LVariable(Name(v))) in
-	match rettype with
+    let body = match rettype with
 	| None ->
 		(* This is a function call with no explicit
 		   return arguments, so just splice the function call in.  *)
@@ -20,10 +32,24 @@ let generate_program_for (apispec: apispec) (iospec: iospec) (pre, post) =
 	| Some(resref) ->
 		Sequence([pre;
 			Assignment(resref, Expression(funcall));
-			post])
+			post]) in
+    let unified_typemap =
+        build_typemap_for apispec iospec body in
+    {
+        in_variables = iospec.livein;
+        gir = body;
+        out_variables = iospec.liveout;
+        typemap = unified_typemap;
+    }
 
 (* Given a set of pre/post pairs, fill thsee out into whole programs
    that can be turned into C code.  *)
-let generate_programs opts classmap (iospec: iospec) (api: apispec)
+let generate_programs (opts: options) classmap (iospec: iospec) (api: apispec)
 	(conversion_functions: (gir * gir) list)  =
-		List.map conversion_functions (generate_program_for api iospec)
+		let result_programs = List.map conversion_functions (generate_program_for api iospec) in
+		let () = if opts.dump_generate_program then
+			Printf.printf "Generated Programs are: %s\n\n" (
+				program_list_to_string result_programs
+			)
+		else () in
+        result_programs
