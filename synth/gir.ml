3b81b014918fe7db119df4a5a8c0841599da9ee0
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-02-09 15:43:09 +0000

    Minor IR changes to represent C

--- synth/gir.ml
+++ synth/gir.ml
@@ -5,26 +5,28 @@ type function_ref = FunctionRef of name_reference
 (* Needs a list name and the variable that we are considering. *)
 type expression =
 	| VariableReference of name_reference
-	| ListIndex of name_reference * expression
+	| ListIndex of expression * expression
 	| FunctionCall of function_ref * varlist
 
 and rvalue =
 	| Expression of expression
-    | RReference of name_reference
 
 and lvalue =
     | LVariable of name_reference
+	(* Variable * index variable *)
+	| LIndex of lvalue * expression
 
 and varlist =
     | VariableList of name_reference list
 
 and gir =
+	| Definition of name_reference
 	| Sequence of gir list
 	(* This can eitehr assign lists to lists, of variables to
 	   variables.  *)
 	| Assignment of lvalue * rvalue
-	(* Body, loop max value *)
-    | LoopOver of gir * name_reference
+	(* Body, induction variable name, loop max value *)
+    | LoopOver of gir * name_reference * name_reference
 	| Expression of expression
 	| EmptyGIR
 	(* (why?) Todo --- add a lambda here *)
--- synth/gir_utils.ml
+++ synth/gir_utils.ml
@@ -5,10 +5,12 @@ open Gir;;
 
 let rec gir_to_string gir =
 	match gir with
+    | Definition(nref) -> "Define " ^ (name_reference_to_string nref)
 	| Sequence(sublist) -> String.concat ~sep:";\n" (List.map sublist gir_to_string)
 	| Assignment(lval, rval) -> (lvalue_to_string lval) ^ " = " ^ (rvalue_to_string rval)
-	| LoopOver(body, limit) ->
+	| LoopOver(body, ind, limit) ->
 			"LoopUpTo " ^ (name_reference_to_string limit) ^
+			" indvar " ^ (name_reference_to_string ind) ^
 			"{\n" ^ (gir_to_string body) ^ "}"
 	| Expression(expr) ->
 			(expression_to_string expr)
@@ -16,14 +18,16 @@ let rec gir_to_string gir =
 and lvalue_to_string lval =
 	match lval with
 	| LVariable(nam) -> (name_reference_to_string nam)
+    | LIndex(lval, ind) ->
+            (lvalue_to_string lval) ^ "[" ^
+            (expression_to_string ind) ^ "]"
 and rvalue_to_string rval =
 	match rval with
 	| Expression(expr) -> (expression_to_string expr)
-	| RReference(nam) -> (name_reference_to_string nam)
 and expression_to_string expr =
 	match expr with
 	| VariableReference(nam) -> (name_reference_to_string nam)
-	| ListIndex(nam, expr) -> (name_reference_to_string nam) ^
+	| ListIndex(nam, expr) -> (expression_to_string nam) ^
 		"[" ^ (expression_to_string expr) ^ "]"
 	| FunctionCall(fref, varlist) ->
 			(function_ref_to_string fref) ^ "(" ^ (varlist_to_string varlist) ^ ")"
