33c4804d0ed5ec97645606caae9aa2000aac2848
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-02-14 01:40:18 +0000

    add testing dependency

--- shell.nix
+++ shell.nix
@@ -2,6 +2,6 @@
 
 with pkgs;
 mkShell {
-	buildInputs = [ ocaml ocamlPackages.findlib ocamlPackages.ocamlbuild ocamlPackages.yojson ocamlPackages.core_kernel opam ocamlPackages.cmdliner cloc ];
+	buildInputs = [ ocaml ocamlPackages.findlib ocamlPackages.ocamlbuild ocamlPackages.yojson ocamlPackages.core_kernel opam ocamlPackages.cmdliner cloc ocamlPackages.ppx_expect ];
 	SHELL_NAME = "FFTSynth";
 }
