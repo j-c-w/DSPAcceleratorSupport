439eb31d04b8c6aea0747545b32a622ab7a4a272
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-02-19 14:46:30 +0000

    Get better stack traces

--- shell.nix
+++ shell.nix
@@ -3,5 +3,7 @@
 with pkgs;
 mkShell {
 	buildInputs = [ ocaml ocamlPackages.findlib ocamlPackages.ocamlbuild ocamlPackages.yojson ocamlPackages.core_kernel opam ocamlPackages.cmdliner cloc ocamlPackages.ppx_expect ];
+	# Enable ocaml stack traces.
+	OCAMLRUNPARAM = "b";
 	SHELL_NAME = "FFTSynth";
 }
