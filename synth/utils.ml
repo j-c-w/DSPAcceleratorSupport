b2a90ff2ed8baa3be06012209150fe1cabcd1dbd
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-02-23 23:53:46 +0000

    Add some utils

--- synth/utils.ml
+++ synth/utils.ml
@@ -16,3 +16,9 @@ let rec cross_product ls =
 			)
 			)
 
+
+let uniq_cons eql x xs = if List.mem xs x eql then xs else x :: xs
+
+let remove_duplicates eql xs = List.fold_right xs ~f:(uniq_cons eql) ~init:[]
+
+let set_difference eql l1 l2 = List.filter l1 (fun x -> not (List.mem l2 x eql))
