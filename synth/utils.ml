663fb2813d45bde3e1d95873aee63a256b1f78f4
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-02-26 19:44:58 +0000

    Add more utils

--- synth/utils.ml
+++ synth/utils.ml
@@ -22,3 +22,20 @@ let uniq_cons eql x xs = if List.mem xs x eql then xs else x :: xs
 let remove_duplicates eql xs = List.fold_right xs ~f:(uniq_cons eql) ~init:[]
 
 let set_difference eql l1 l2 = List.filter l1 (fun x -> not (List.mem l2 x eql))
+
+let hash_table_from_list s l =
+    let tbl = Hashtbl.create s in
+    let () = ignore(List.map l (fun (i, t) ->
+        Hashtbl.add tbl i t
+    )) in
+    tbl
+
+let rec truncate_zip l1 l2 =
+	match l1, l2 with
+	| [], _ -> []
+	| _, [] -> []
+	| x :: xs, y :: ys ->
+			(x, y) :: (truncate_zip xs ys)
+
+let prepend_all x xs =
+    List.map xs (fun l -> x :: l)
