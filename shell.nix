4e9ccb22797192bc0a238c739b8b4555699c23ba
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-03-20 20:07:34 +0000

    Add alcotest and also racket (not currently used)

--- shell.nix
+++ shell.nix
@@ -3,11 +3,12 @@
 with pkgs;
 mkShell {
 	buildInputs = [
-		cloc ocaml gdb
+		cloc ocaml gdb racket
 		# Ocaml packages
 		ocamlPackages.findlib ocamlPackages.ocamlbuild ocamlPackages.yojson
 		ocamlPackages.core_kernel opam ocamlPackages.cmdliner
 		ocamlPackages.ppx_expect ocamlPackages.parmap
+		ocamlPackages.alcotest
 		# C++ deps for the synnthesizer
 		gcc nlohmann_json
 		# Other indirect deps for the synthesizer.
@@ -20,4 +21,7 @@ mkShell {
 	# Enable ocaml stack traces.
 	OCAMLRUNPARAM = "b";
 	SHELL_NAME = "FFTSynth";
+	shellHook = ''
+			raco pkg install rosette
+		'';
 }
