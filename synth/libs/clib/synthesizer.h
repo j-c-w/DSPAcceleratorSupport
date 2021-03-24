5a9a71499e14b6566dcb15366c610cdb5b203449
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-03-24 11:08:15 +0000

    Use equals rather than assign :)

--- synth/libs/clib/synthesizer.h
+++ synth/libs/clib/synthesizer.h
@@ -1,8 +1,8 @@
-#define POWER_OF_TWO(x) ((x & (x - `1)) == 0)
+#define POWER_OF_TWO(x) ((x & (x - 1)) == 0)
 #define GREATER_THAN(x, y) x > y
 #define GREATER_THAN_OR_EQUAL(x, y) x >= y
 #define LESS_THAN(x, y) x < y
 #define LESS_THAN_OR_EQUAL(x, y) x <= y
-#define EQUAL(x, y) x = y
+#define PRIM_EQUAL(x, y) x == y
 /* TODO --- would like to make this better.  */
 #define FLOAT_EQUAL(x, y) ((x < y + x / 1000.0) && (x > y - x / 1000.0))
