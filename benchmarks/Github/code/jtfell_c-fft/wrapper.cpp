49faffd17e2117e3f3cd87007c02fa94cd2eccb9
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-05-21 12:45:39 +0100

    Update iospec and wrapper as appropriate to support return values

--- benchmarks/Github/code/jtfell_c-fft/wrapper.cpp
+++ benchmarks/Github/code/jtfell_c-fft/wrapper.cpp
@@ -11,12 +11,27 @@ extern "C" {
 char *output_file; 
 char *pre_accel_dump_file; // optional dump file. 
 using json = nlohmann::json;
-void write_output(complex * x, int N) {
+
+
+std::chrono::steady_clock::time_point AcceleratorStart;
+long long AcceleratorTotalNanos = 0;
+void StartAcceleratorTimer() {
+	AcceleratorStart = std::chrono::steady_clock::now();
+}
+
+void StopAcceleratorTimer() {
+	AcceleratorTotalNanos +=
+		std::chrono::duration_cast<std::chrono::nanoseconds>(
+			std::chrono::steady_clock::now() - AcceleratorStart
+		).count();
+}
+
+void write_output(complex * x, int N, complex * returnv) {
 
     json output_json;
 std::vector<json> output_temp_1;
 for (unsigned int i2 = 0; i2 < N; i2++) {
-complex output_temp_3 = x[i2];
+complex output_temp_3 = returnv[i2];
 json output_temp_4;
 
 output_temp_4["re"] = output_temp_3.re;
@@ -24,7 +39,7 @@ output_temp_4["re"] = output_temp_3.re;
 output_temp_4["im"] = output_temp_3.im;
 output_temp_1.push_back(output_temp_4);
 }
-output_json["x"] = output_temp_1;
+output_json["returnv"] = output_temp_1;
 std::ofstream out_str(output_file); 
 out_str << std::setw(4) << output_json << std::endl;
 }
@@ -45,8 +60,9 @@ x_vec.push_back(x_inner);
 complex *x = &x_vec[0];
 int N = input_json["N"];
 std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-DFT_naive(x, N);
+complex * returnv = DFT_naive(x, N);
 std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 std::cout << "Time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count() << std::endl;
-write_output(x, N);
+std::cout << "AccTime: " << AcceleratorTotalNanos << std::endl;
+write_output(x, N, returnv);
 }
