fec394d13bffe29ccd1e8bc196acd0fad3c4a813
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2021-05-31 19:11:46 +0100

    Fix utilities to work with the new JSON flie formats

--- synth/utilities/json_gen.ml
+++ synth/utilities/json_gen.ml
@@ -18,7 +18,7 @@ interface.
 The idea is to pass in the IOSpec that it needs to handle,
 this this produces some code that goes around that as a wrapper.
 *)
-let main iospec_file classspec_file output_file =
+let main iospec_file output_file =
     (* Yes, this is a terrible hack I am 100% going to regret
     because I have no clear understanding why it needs both.  *)
     let options = { default_options with generate_timing_code = true } in
@@ -60,18 +60,15 @@ let main iospec_file classspec_file output_file =
 let iospec =
 	let doc = "IO Spec for the Function" in
 	Arg.(required & pos 0 (some string) None & info [] ~docv:"IOSpec" ~doc)
-let classspec =
-	let doc = "Class Specu for the types involved" in
-	Arg.(required & pos 1 (some string) None & info [] ~docv:"Classspec" ~doc)
 
 let outfile =
 	let doc = "Output file for the function wrap" in
-	Arg.(required & pos 2 (some string) None & info [] ~docv:"Outfile" ~doc)
+	Arg.(required & pos 1 (some string) None & info [] ~docv:"Outfile" ~doc)
 
 let info =
 	let doc = "Generate JSON wrappers" in
 	Term.info "JSONGen" ~doc
 
-let args_t = Term.(const main $ iospec $ classspec $ outfile)
+let args_t = Term.(const main $ iospec $ outfile)
 
 let () = Term.exit @@ Term.eval (args_t, info)
