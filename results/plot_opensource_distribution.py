a89fd8b4627c47dd94dec5d876678f639729e4a2
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-06-01 08:59:57 +0100

    Add a script to plot the project distribution

---
+++ results/plot_opensource_distribution.py
@@ -0,0 +1,39 @@
+import argparse
+import matplotlib.pyplot as plt
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('InfoFile')
+    args = parser.parse_args()
+
+    total_size = 0
+    with open(args.InfoFile) as f:
+        reasons = {}
+        for line in f.readlines():
+            print (line)
+            reason = line.split(":")[2].strip()
+            if reason in reasons:
+                reasons[reason] += 1
+                total_size += 1
+            else:
+                reasons[reason] = 1
+                total_size += 1
+
+    sizes = []
+    explode = []
+    labels = []
+
+    for key in sorted(reasons.keys()):
+        if key == "Supported":
+            explode.append(0.1)
+        else:
+            explode.append(0.0)
+
+        sizes.append(float(reasons[key]) / float(total_size))
+        labels.append(key)
+
+    fig, ax = plt.subplots()
+    ax.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
+    ax.axis('equal')
+
+    plt.savefig('open_source_project_distribution.eps')
