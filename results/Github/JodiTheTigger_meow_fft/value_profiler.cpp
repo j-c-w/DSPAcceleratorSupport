9037de631ba8f4b30deb400e29a582d296507161
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2021-06-04 19:14:07 +0100

    Add initial value_profiler for another project

---
+++ results/Github/JodiTheTigger_meow_fft/value_profiler.cpp
@@ -0,0 +1,133 @@
+#include<vector>
+#include<nlohmann/json.hpp>
+#include<fstream>
+#include<iomanip>
+#include<clib/synthesizer.h>
+#include<time.h>
+#include<iostream>
+char *output_file; 
+char *pre_accel_dump_file; // optional dump file. 
+using json = nlohmann::json;
+const char* __asan_default_options() { return "detect_leaks=0"; }
+
+
+clock_t AcceleratorStart;
+clock_t AcceleratorTotalNanos = 0;
+void StartAcceleratorTimer() {
+	AcceleratorStart = clock();
+}
+
+void StopAcceleratorTimer() {
+	AcceleratorTotalNanos +=
+		(clock()) - AcceleratorStart;
+}
+
+void write_output(Meow_FFT_Workset * data, Meow_FFT_Complex * in, Meow_FFT_Complex * out) {
+
+    json output_json;
+Meow_FFT_Workset output_temp_1 = *data;
+json output_temp_2;
+
+output_temp_2["N"] = output_temp_1->N;
+std::vector<json> output_temp_3;
+for (unsigned int i4 = 0; i4 < output_temp_1->N; i4++) {
+Meow_FFT_Complex output_temp_5 = wn[i4];
+json output_temp_6;
+
+output_temp_6["r"] = output_temp_5.r;
+
+output_temp_6["j"] = output_temp_5.j;
+output_temp_3.push_back(output_temp_6);
+}
+output_temp_2["wn"] = output_temp_3;
+std::vector<json> output_temp_7;
+for (unsigned int i8 = 0; i8 < output_temp_1->N; i8++) {
+Meow_FFT_Complex output_temp_9 = wn_ordered[i8];
+json output_temp_10;
+
+output_temp_10["r"] = output_temp_9.r;
+
+output_temp_10["j"] = output_temp_9.j;
+output_temp_7.push_back(output_temp_10);
+}
+output_temp_2["wn_ordered"] = output_temp_7;
+json output_temp_11;
+
+output_temp_11["count"] = stages.count;
+std::vector<json> output_temp_12;
+for (unsigned int i13 = 0; i13 < stages.count; i13++) {
+unsigned int output_temp_14 = radix[i13];
+
+output_temp_12.push_back(stages.output_temp_14);
+}
+output_temp_11["radix"] = output_temp_12;
+std::vector<json> output_temp_15;
+for (unsigned int i16 = 0; i16 < stages.count; i16++) {
+unsigned int output_temp_17 = remainder[i16];
+
+output_temp_15.push_back(stages.output_temp_17);
+}
+output_temp_11["remainder"] = output_temp_15;
+std::vector<json> output_temp_18;
+for (unsigned int i19 = 0; i19 < stages.count; i19++) {
+unsigned int output_temp_20 = offsets[i19];
+
+output_temp_18.push_back(stages.output_temp_20);
+}
+output_temp_11["offsets"] = output_temp_18;
+output_temp_2["stages"] = output_temp_11;
+output_json["data"] = output_temp_2;
+/* Don't wnat to profile input data.  */
+/* std::vector<json> output_temp_21; */
+/* for (unsigned int i22 = 0; i22 < N; i22++) { */
+/* Meow_FFT_Complex output_temp_23 = in[i22]; */
+/* json output_temp_24; */
+
+/* output_temp_24["r"] = output_temp_23.r; */
+
+/* output_temp_24["j"] = output_temp_23.j; */
+/* output_temp_21.push_back(output_temp_24); */
+/* } */
+/* output_json["in"] = output_temp_21; */
+std::ofstream out_str(output_file); 
+out_str << std::setw(4) << output_json << std::endl;
+}
+int main(int argc, char **argv) {
+    char *inpname = argv[1]; 
+    output_file = argv[2]; 
+
+    std::ifstream ifs(inpname); 
+    json input_json = json::parse(ifs);
+Meow_Fft_Stages data_pointerstages = { data_pointerstagescount, data_pointerstagesradix, data_pointerstagesremainder, data_pointerstagesoffsets};
+Meow_FFT_Workset data_pointer = { data_pointerN, data_pointerwn, data_pointerwn_ordered, data_pointerstages};
+Meow_FFT_Workset* data = &data_pointer;
+std::vector<Meow_FFT_Complex> in_vec;
+int n = 0;
+for (auto& elem : input_json["in"]) {
+float in_innerr = elem["r"];
+float in_innerj = elem["j"];
+Meow_FFT_Complex in_inner = { in_innerr, in_innerj};
+in_vec.push_back(in_inner);
+n += 1;
+}
+
+
+// begin typical use (hand-written)
+// Typical use from the github example: https://github.com/JodiTheTigger/meow_fft
+size_t workset_bytes = meow_fft_generate_workset_complex(n, NULL);
+Meow_FFT_Workset_Complex fft_complex =
+	(Meow_FFT_Workset_Complex *) malloc(workset_bytes);
+meow_fft_generate_workset_complex(n, fft_complex);
+Meow_FFT_Complex *in = &in_vec[0];
+Meow_FFT_Complex out[n];
+clock_t begin = clock();
+meow_fft_complex(data, in, out);
+clock_t end = clock();
+
+
+
+
+std::cout << "Time: " << (double) (end - begin) / CLOCKS_PER_SEC << std::endl;
+std::cout << "AccTime: " << (double) AcceleratorTotalNanos / CLOCKS_PER_SEC << std::endl;
+write_output(data, in, out);
+}
