b2e8037d6c17833cdd12fefed4fd14775f7e2982
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-06-07 16:58:12 +0100

    Add support for inserts

--- benchmarks/Github/code/extractor.sh
+++ benchmarks/Github/code/extractor.sh
@@ -5,6 +5,7 @@ if [[ $# -ne 1 ]]; then
 	echo "Usage: $0 <extractor file format>"
 	echo "Extractor file format is:"
 	echo "one entry per line, format is 'filename: <lineno start>,<lineno end> <#optional comment>'"
+	echo "or: INSERT: contents"
 	exit 1
 fi
 
@@ -12,5 +13,10 @@ IFS=$'\n'
 for line in $(cat $1); do
 	fname=$(echo $line | cut -f1 -d ':')
 	range=$(echo $line | cut -f2 -d ' ')
-	sed -n "${range}p" $fname
+	if [[ $fname == *INSERT* ]]; then
+		contents=$(echo $line | cut -f2- -d:)
+		echo "$contents"
+	else
+		sed -n "${range}p" $fname
+	fi
 done
