f91f5a7f56f6bc2d422c0745b5c08219ec111f02
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-06-07 12:28:14 +0100

    Regenerate the wrapper with proper pointer support

--- benchmarks/Github/code/JodiTheTigger_meow_fft/wrapper.cpp
+++ benchmarks/Github/code/JodiTheTigger_meow_fft/wrapper.cpp
@@ -3,15 +3,28 @@
 #include<fstream>
 #include<iomanip>
 #include<clib/synthesizer.h>
-#include<chrono>
-#include<iostream>
 #include<time.h>
+#include<iostream>
 extern "C" {
 #include "self_contained_code.c"
 }
 char *output_file; 
 char *pre_accel_dump_file; // optional dump file. 
 using json = nlohmann::json;
+const char* __asan_default_options() { return "detect_leaks=0"; }
+
+
+clock_t AcceleratorStart;
+clock_t AcceleratorTotalNanos = 0;
+void StartAcceleratorTimer() {
+	AcceleratorStart = clock();
+}
+
+void StopAcceleratorTimer() {
+	AcceleratorTotalNanos +=
+		(clock()) - AcceleratorStart;
+}
+
 void write_output(Meow_FFT_Workset * data, Meow_FFT_Complex * in, Meow_FFT_Complex * out) {
 
     json output_json;
@@ -29,56 +42,51 @@ output_json["out"] = output_temp_1;
 std::ofstream out_str(output_file); 
 out_str << std::setw(4) << output_json << std::endl;
 }
-
 int main(int argc, char **argv) {
     char *inpname = argv[1]; 
     output_file = argv[2]; 
 
     std::ifstream ifs(inpname); 
     json input_json = json::parse(ifs);
-std::vector<Meow_FFT_Workset> data_vec;
-for (auto& elem : input_json["data"]) {
-int data_innerN = elem["N"];
-std::vector<Meow_FFT_Complex> data_innerwn_vec;
-for (auto& elem : elem["wn"]) {
-float data_innerwn_innerr = elem["r"];
-float data_innerwn_innerj = elem["j"];
-Meow_FFT_Complex data_innerwn_inner = { data_innerwn_innerr, data_innerwn_innerj};
-data_innerwn_vec.push_back(data_innerwn_inner);
+int data_pointerN = input_json["data"]["N"];
+std::vector<Meow_FFT_Complex> data_pointerwn_vec;
+for (auto& elem : input_json["data"]["wn"]) {
+float data_pointerwn_innerr = elem["r"];
+float data_pointerwn_innerj = elem["j"];
+Meow_FFT_Complex data_pointerwn_inner = { data_pointerwn_innerr, data_pointerwn_innerj};
+data_pointerwn_vec.push_back(data_pointerwn_inner);
 }
-Meow_FFT_Complex *data_innerwn = &data_innerwn_vec[0];
-std::vector<Meow_FFT_Complex> data_innerwn_ordered_vec;
-for (auto& elem : elem["wn_ordered"]) {
-float data_innerwn_ordered_innerr = elem["r"];
-float data_innerwn_ordered_innerj = elem["j"];
-Meow_FFT_Complex data_innerwn_ordered_inner = { data_innerwn_ordered_innerr, data_innerwn_ordered_innerj};
-data_innerwn_ordered_vec.push_back(data_innerwn_ordered_inner);
+Meow_FFT_Complex *data_pointerwn = &data_pointerwn_vec[0];
+std::vector<Meow_FFT_Complex> data_pointerwn_ordered_vec;
+for (auto& elem : input_json["data"]["wn_ordered"]) {
+float data_pointerwn_ordered_innerr = elem["r"];
+float data_pointerwn_ordered_innerj = elem["j"];
+Meow_FFT_Complex data_pointerwn_ordered_inner = { data_pointerwn_ordered_innerr, data_pointerwn_ordered_innerj};
+data_pointerwn_ordered_vec.push_back(data_pointerwn_ordered_inner);
 }
-Meow_FFT_Complex *data_innerwn_ordered = &data_innerwn_ordered_vec[0];
-unsigned int data_innerstagescount = elem["stages"]["count"];
-std::vector<unsigned int> data_innerstagesradix_vec;
-for (auto& elem : elem["stages"]["radix"]) {
-unsigned int data_innerstagesradix_inner = elem;
-data_innerstagesradix_vec.push_back(data_innerstagesradix_inner);
+Meow_FFT_Complex *data_pointerwn_ordered = &data_pointerwn_ordered_vec[0];
+unsigned int data_pointerstagescount = input_json["data"]["stages"]["count"];
+std::vector<unsigned int> data_pointerstagesradix_vec;
+for (auto& elem : input_json["data"]["stages"]["radix"]) {
+unsigned int data_pointerstagesradix_inner = elem;
+data_pointerstagesradix_vec.push_back(data_pointerstagesradix_inner);
 }
-unsigned int *data_innerstagesradix = &data_innerstagesradix_vec[0];
-std::vector<unsigned int> data_innerstagesremainder_vec;
-for (auto& elem : elem["stages"]["remainder"]) {
-unsigned int data_innerstagesremainder_inner = elem;
-data_innerstagesremainder_vec.push_back(data_innerstagesremainder_inner);
+unsigned int *data_pointerstagesradix = &data_pointerstagesradix_vec[0];
+std::vector<unsigned int> data_pointerstagesremainder_vec;
+for (auto& elem : input_json["data"]["stages"]["remainder"]) {
+unsigned int data_pointerstagesremainder_inner = elem;
+data_pointerstagesremainder_vec.push_back(data_pointerstagesremainder_inner);
 }
-unsigned int *data_innerstagesremainder = &data_innerstagesremainder_vec[0];
-std::vector<unsigned int> data_innerstagesoffsets_vec;
-for (auto& elem : elem["stages"]["offsets"]) {
-unsigned int data_innerstagesoffsets_inner = elem;
-data_innerstagesoffsets_vec.push_back(data_innerstagesoffsets_inner);
+unsigned int *data_pointerstagesremainder = &data_pointerstagesremainder_vec[0];
+std::vector<unsigned int> data_pointerstagesoffsets_vec;
+for (auto& elem : input_json["data"]["stages"]["offsets"]) {
+unsigned int data_pointerstagesoffsets_inner = elem;
+data_pointerstagesoffsets_vec.push_back(data_pointerstagesoffsets_inner);
 }
-unsigned int *data_innerstagesoffsets = &data_innerstagesoffsets_vec[0];
-Meow_Fft_Stages data_innerstages = { data_innerstagescount, data_innerstagesradix, data_innerstagesremainder, data_innerstagesoffsets};
-Meow_FFT_Workset data_inner = { data_innerN, data_innerwn, data_innerwn_ordered, data_innerstages};
-data_vec.push_back(data_inner);
-}
-Meow_FFT_Workset *data = &data_vec[0];
+unsigned int *data_pointerstagesoffsets = &data_pointerstagesoffsets_vec[0];
+Meow_Fft_Stages data_pointerstages = { data_pointerstagescount, data_pointerstagesradix, data_pointerstagesremainder, data_pointerstagesoffsets};
+Meow_FFT_Workset data_pointer = { data_pointerN, data_pointerwn, data_pointerwn_ordered, data_pointerstages};
+Meow_FFT_Workset* data = &data_pointer;
 std::vector<Meow_FFT_Complex> in_vec;
 for (auto& elem : input_json["in"]) {
 float in_innerr = elem["r"];
@@ -94,5 +102,6 @@ for (int i = 0; i < TIMES; i ++) {
 }
 clock_t end = clock();
 std::cout << "Time: " << (double) (end - begin) / CLOCKS_PER_SEC << std::endl;
+std::cout << "AccTime: " << (double) AcceleratorTotalNanos / CLOCKS_PER_SEC << std::endl;
 write_output(data, in, out);
 }
