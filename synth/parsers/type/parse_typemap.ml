4be8ab43dcfcc9c9a2f8955544114f5811500b54
Jackson Woodruff <woodruff.jackson@gmail.com>
2021-07-02 18:36:49 +0100

    Add more helpful debug message when parsing

--- synth/parsers/type/parse_typemap.ml
+++ synth/parsers/type/parse_typemap.ml
@@ -5,6 +5,8 @@ open Spec_definition;;
 open Parse_type;;
 open Options;;
 
+exception LoadTypemapException of string
+
 let load_typemap options json_definition typenames =
 	let () = if options.debug_load then
 		Printf.printf "Loading names %s into typemap\n" (String.concat ~sep:", " typenames)
@@ -12,7 +14,13 @@ let load_typemap options json_definition typenames =
 	in
 	let typemap = Hashtbl.create (module String) in
 	(* Get the types parsed *)
-	let typemap_pairs = List.map typenames (fun name -> (name, json_definition |> member "typemap" |> member name |> to_string |> parse_type)) in
+	let json_mapping = json_definition |> member "typemap" in
+	let typemap_pairs = List.map typenames (fun name ->
+		if (Yojson.Basic.Util.member name json_mapping) = `Null then
+			raise (LoadTypemapException ("Not found type for variable " ^ name ^ "\n"))
+		else
+			(name, json_mapping |> member name |> to_string |> parse_type)
+	) in
 	(* Put the parsed types in a hash table. *)
 	ignore(List.map typemap_pairs (fun (name, typ) -> Hashtbl.add typemap name typ));
 	typemap
