5fe1e1e19d97b07a1d8f6049327f5eede617c2c5
Jackson Woodruff <J.C.Woodruff@sms.ed.ac.uk>
2021-07-30 17:41:42 +0100

    Add more results

--- results/Github/tasimon_FFT/generate_inputs.sh
+++ results/Github/tasimon_FFT/generate_inputs.sh
@@ -17,18 +17,18 @@ pow2len=$(echo $inplen | awk '{print log($inplen)/log(2)}')
 if [[ $pow2len =~ ^[0-9]+\.[0-9]*$ ]]; then # Match floating points
 	echo "$_num is not an exponent of 2"; # Not exponent if floating point
 	# https://stackoverflow.com/questions/48910300/how-to-find-values-2-exponential-in-shell
-	exit 1
+	exit 0
 fi
 
 if (( $inplen != 64 )); then
 	echo "This FFT only supports length 64"
-	exit 1
+	exit 0
 fi
 
 mkdir -p inputs
 # I haven't yet seen a data-dependent FFT algorithm,
 # so we'll just use a constnat input.
-contents=", 2.510045379820, 21.72443302820"
+contents="2.510045379820, 21.72443302820"
 typeset -a inputs
 inputs=("$contents")
 for i in $(seq 1 $(( $inplen - 1 ))); do
